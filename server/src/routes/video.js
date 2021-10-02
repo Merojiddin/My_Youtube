fee32124cbcc67003becd91bb45adb4356f3337e
Merojiddin <merojiddin_1998@mail.ru>
2021-10-02 14:57:55 +0800

    delete comment added

--- server/src/routes/video.js
+++ server/src/routes/video.js
@@ -1,31 +1,200 @@
-import express from "express";
+import express, { response } from "express";
+import { PrismaClient } from "@prisma/client"
+import { protect } from "../middleware/authorization";
+
+const prisma = new PrismaClient()
 
 function getVideoRoutes() {
   const router = express.Router();
 
+  router.get('/', getRecommendedVideos)
+  router.get('/trending', getTrendingVideos)
+  router.get('/search', searchVideos)
+
+  router.post('/', protect, addVideo)
+  router.post('/:videoId/comment', protect, addComment)
+  router.delete('/:videoId/comment/:commentId', protect, deleteComment)
+
   return router;
 }
 
-async function getRecommendedVideos(req, res) {}
+async function getVideoViews(videos) {
+  for (const video of videos) {
+    const views = await prisma.view.count({
+      where: {
+        videoId: {
+          equals: video.id
+        }
+      }
+    })
+    video.views = views
+  }
+  return videos
+}
 
-async function getTrendingVideos(req, res) {}
+async function getRecommendedVideos(req, res) {
+  let videos = await prisma.video.findMany({
+    include: {
+      user: true
+    },
+    orderBy: {
+      createdAt: "desc"
+    }
+  })
 
-async function searchVideos(req, res, next) {}
+  if (!videos.length) {
+    return res.status(200).json({ videos })
 
-async function addVideo(req, res) {}
+  }
+  videos = await getVideoViews(videos)
+  res.status(200).json({ videos })
 
-async function addComment(req, res, next) {}
+}
 
-async function deleteComment(req, res) {}
+async function getTrendingVideos(req, res) {
+  let videos = await prisma.video.findMany({
+    include: {
+      user: true
+    },
+    orderBy: {
+      createdAt: "desc"
+    }
+  })
 
-async function addVideoView(req, res, next) {}
+  if (!videos.length) {
+    return res.status(200).json({ videos })
 
-async function likeVideo(req, res, next) {}
+  }
+  videos = await getVideoViews(videos)
+  videos.sort((a, b) => b.views - a.views)
+  res.status(200).json({ videos })
 
-async function dislikeVideo(req, res, next) {}
+}
 
-async function getVideo(req, res, next) {}
+async function searchVideos(req, res, next) {
+  if (!req.query.query) {
+    return next({
+      statusCode: 400,
+      message: "You haven`t entered the serachasd fasdf afgnkdcvbrjnbfjtbg"
+    })
+  }
 
-async function deleteVideo(req, res) {}
+  let videos = await prisma.video.findMany({
+    include: {
+      user: true
+    },
+    where: {
+      OR: [
+        {
+          title: {
+            contains: req.query.query,
+            mode: "insensitive"
+          }
+        },
+        {
+          description: {
+            contains: req.query.query,
+            mode: "insensitive"
+          },
+        }
+      ]
+    }
+  })
+
+  if (!videos.length) {
+    return res.status(200).json({ videos })
+
+  }
+  videos = await getVideoViews(videos)
+  res.status(200).json({ videos })
+
+}
+
+async function addVideo(req, res) {
+  const { title, description, url, thumbnail } = req.body;
+
+  const video = await prisma.video.create({
+    data: {
+      title, description, url, thumbnail,
+      user: {
+        connect: {
+          id: req.user.id
+        }
+      }
+    }
+  })
+
+  res.status(200).json({ video })
+}
+
+async function addComment(req, res, next) {
+  const video = await prisma.video.findUnique({
+    where: {
+      id: req.params.videoId
+    }
+  })
+
+  if (!video) {
+    return next({
+      message: `The video with ${req.params.videoId} doesn't exist`,
+      statusCode: 404
+    })
+  }
+
+  const comment = await prisma.comment.create({
+    data: {
+      text: req.body.text,
+      user: {
+        connect: {
+          id: req.user.id
+        }
+      },
+      video: {
+        connect: {
+          id: req.params.videoId
+        }
+      }
+    }
+  })
+
+  res.status(200).json({ comment })
+}
+
+async function deleteComment(req, res) {
+  const comment = await prisma.comment.findUnique({
+    where: {
+      id: req.params.commentId
+    },
+    select: {
+      userId: true
+    }
+  })
+
+  if (comment.userId !== req.user.id) {
+    return res.status(401).send("You are not authorised to delete this comment")
+  }
+
+  await prisma.comment.delete({
+    where: {
+      id: req.params.commentId
+    }
+  });
+
+  res.status(200).json({}).send()
+}
+
+async function addVideoView(req, res, next) {
+
+}
+
+async function likeVideo(req, res, next) {
+
+}
+
+async function dislikeVideo(req, res, next) { }
+
+async function getVideo(req, res, next) { }
+
+async function deleteVideo(req, res) { }
 
 export { getVideoRoutes };
